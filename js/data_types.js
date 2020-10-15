8485773becd1460e6332a16a25336b25e2340c4e
sirko1985 <itruhan@gmail.com>
2020-10-15 21:23:39 +0300

    task 2

--- js/data_types.js
+++ js/data_types.js
@@ -1,4 +1,8 @@
 // 4.1.1
 function camelize(str) {
    return str.split('-').map((word, index) => index == 0 ? word : word[0].toUpperCase() + word.slice).join('');
+};
+
+function filterRange(arr, a, b) {
+   return arr.filter(item => (a <= item && item <= b));
 }
